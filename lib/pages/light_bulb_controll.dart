cb8c953560ba012748c729407b222f5edb675a10
Hailemariyam <haile0938048261kebede@gmail.com>
2024-05-29 22:58:15 -0700

    pubspec

--- lib/pages/light_bulb_controll.dart
+++ lib/pages/light_bulb_controll.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:http/http.dart' as http;
 
 class LightControlScreen extends StatefulWidget {
   @override
@@ -8,36 +9,63 @@ class LightControlScreen extends StatefulWidget {
 
 class _LightControlScreenState extends State<LightControlScreen> {
   bool isLightOn = false;
+  String offUrl = "http://192.168.0.3/26/off";
+  String onUrl = "http://192.168.0.3/26/on";
 
   @override
   void initState() {
     super.initState();
-    _fetchLightStatus();
+    // _fetchLightStatus();
   }
 
-  void _fetchLightStatus() async {
-    DocumentSnapshot snapshot = await FirebaseFirestore.instance
-        .collection('users')
-        .doc('user_id')
-        .collection('devices')
-        .doc('light_bulb')
-        .get();
-    setState(() {
-      isLightOn = snapshot['status'] == 'on';
-    });
-  }
+  // void _fetchLightStatus() async {
+  //   DocumentSnapshot snapshot = await FirebaseFirestore.instance
+  //       .collection('users')
+  //       .doc('user_id')
+  //       .collection('devices')
+  //       .doc('light_bulb')
+  //       .get();
+  //   setState(() {
+  //     isLightOn = snapshot['status'] == 'on';
+  //   });
+  // }
 
-  void _toggleLight() async {
-    setState(() {
-      isLightOn = !isLightOn;
-    });
-    await FirebaseFirestore.instance
-        .collection('users')
-        .doc('user_id')
-        .collection('devices')
-        .doc('light_bulb')
-        .set({'status': isLightOn ? 'on' : 'off'});
-  }
+  // void _toggleLight() async {
+  //   setState(() {
+  //     isLightOn = !isLightOn;
+  //   });
+  //   await FirebaseFirestore.instance
+  //       .collection('users')
+  //       .doc('user_id')
+  //       .collection('devices')
+  //       .doc('light_bulb')
+  //       .set({'status': isLightOn ? 'on' : 'off'});
+  // }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: Center(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: <Widget>[
+//             Text(
+//               'Light is ${isLightOn ? "On" : "Off"}',
+//               style: TextStyle(fontSize: 24),
+//             ),
+//             SizedBox(height: 20),
+//             Switch(
+//               value: isLightOn,
+//               onChanged: (value) {
+//                 _toggleLight();
+//               },
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
 
   @override
   Widget build(BuildContext context) {
@@ -47,7 +75,7 @@ class _LightControlScreenState extends State<LightControlScreen> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Text(
-              'Light is ${isLightOn ? "On" : "Off"}',
+              '',
               style: TextStyle(fontSize: 24),
             ),
             SizedBox(height: 20),
@@ -61,5 +89,21 @@ class _LightControlScreenState extends State<LightControlScreen> {
         ),
       ),
     );
+  }
+
+  void _toggleLight() async {
+    setState(() {
+      isLightOn = !isLightOn;
+      callRest(isLightOn);
+    });
+  }
+
+  void callRest(isLightOn) {
+    if (isLightOn) {
+      http.get(Uri.parse(onUrl));
+    }
+    if (!isLightOn) {
+      http.get(Uri.parse(offUrl));
+    }
   }
 }
