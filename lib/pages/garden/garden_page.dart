6b105fb8f02afddb60ba4ef8ce645fad2dfece80
BiniyamLamesgen <biniyamlamesgen69@gmail.com>
2024-05-30 03:47:58 +0300

    added features now

--- lib/pages/garden/garden_page.dart
+++ lib/pages/garden/garden_page.dart
@@ -1,31 +1,84 @@
 import 'package:flutter/material.dart';
-import 'dart:ui'; // Import the dart:ui library for ImageFilter
 import 'package:smart_home/components/mymenu.dart';
-import 'package:smart_home/pages/ac_controll.dart';
-import 'package:smart_home/pages/light_bulb_controll.dart';
-import 'package:smart_home/pages/stove_controlle.dart';
+import 'dart:ui'; // Import the dart:ui library for ImageFilter
 
-class GardenControl extends StatelessWidget {
+class GardenControl extends StatefulWidget {
+  @override
+  _GardenControlState createState() => _GardenControlState();
+}
+
+class _GardenControlState extends State<GardenControl> {
   final List<Map<String, dynamic>> menuItems = [
     {
-      'title': 'Light Control',
-      'icon': Icons.lightbulb,
+      'title': 'Garden Lights',
+      'icon': Icons.wb_sunny,
       'warna': Colors.yellow,
-      'route': LightControlScreen(),
+      'control': 'garden_lights',
     },
     {
-      'title': 'AC Control',
-      'icon': Icons.ac_unit,
+      'title': 'Sprinklers',
+      'icon': Icons.opacity,
       'warna': Colors.blue,
-      'route': ACControlScreen(),
+      'control': 'sprinklers',
     },
     {
-      'title': 'Stove Control',
-      'icon': Icons.kitchen,
-      'warna': Colors.red,
-      'route': StoveControlScreen(),
+      'title': 'Fountain',
+      'icon': Icons.pool,
+      'warna': Colors.green,
+      'control': 'fountain',
     },
   ];
+
+  bool _showGardenLightsControl = false;
+  bool _showSprinklersControl = false;
+  bool _showFountainControl = false;
+  bool _isGardenLightsOn = false;
+  bool _isSprinklersOn = false;
+  bool _isFountainOn = false;
+
+  void _toggleControl(String control) {
+    setState(() {
+      if (control == 'garden_lights') {
+        _showGardenLightsControl = !_showGardenLightsControl;
+        _showSprinklersControl = false;
+        _showFountainControl = false;
+      } else if (control == 'sprinklers') {
+        _showSprinklersControl = !_showSprinklersControl;
+        _showGardenLightsControl = false;
+        _showFountainControl = false;
+      } else if (control == 'fountain') {
+        _showFountainControl = !_showFountainControl;
+        _showGardenLightsControl = false;
+        _showSprinklersControl = false;
+      }
+    });
+  }
+
+  void _toggleGardenLights() {
+    setState(() {
+      _isGardenLightsOn = !_isGardenLightsOn;
+    });
+  }
+
+  void _toggleSprinklers() {
+    setState(() {
+      _isSprinklersOn = !_isSprinklersOn;
+    });
+  }
+
+  void _toggleFountain() {
+    setState(() {
+      _isFountainOn = !_isFountainOn;
+    });
+  }
+
+  void _cancelControl() {
+    setState(() {
+      _showGardenLightsControl = false;
+      _showSprinklersControl = false;
+      _showFountainControl = false;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -36,36 +89,38 @@ class GardenControl extends StatelessWidget {
           Container(
             decoration: BoxDecoration(
               image: DecorationImage(
-                image: NetworkImage('https://source.unsplash.com/random'),
+                image:
+                    NetworkImage('https://source.unsplash.com/random?nature'),
                 fit: BoxFit.cover,
               ),
             ),
           ),
           // Glassmorphic Container
-          Column(
-            children: [
-              const SizedBox(height: 10),
-              Expanded(
-                child: Padding(
+          Center(
+            child: ClipRRect(
+              borderRadius: BorderRadius.only(
+                topRight: Radius.circular(60),
+              ),
+              child: BackdropFilter(
+                filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
+                child: Container(
+                  width: double.infinity,
                   padding: const EdgeInsets.all(20.0),
-                  child: ClipRRect(
+                  decoration: BoxDecoration(
+                    color: Colors.white.withOpacity(0.2),
                     borderRadius: BorderRadius.only(
                       topRight: Radius.circular(60),
                     ),
-                    child: BackdropFilter(
-                      filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
-                      child: Container(
-                        width: double.infinity,
-                        height: MediaQuery.of(context).size.height / 1.75,
-                        decoration: BoxDecoration(
-                          color: Colors.white.withOpacity(0.2),
-                          borderRadius: BorderRadius.only(
-                            topRight: Radius.circular(60),
-                          ),
-                          border: Border.all(
-                            color: Colors.white.withOpacity(0.3),
-                          ),
-                        ),
+                    border: Border.all(
+                      color: Colors.white.withOpacity(0.3),
+                    ),
+                  ),
+                  child: Column(
+                    children: [
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Expanded(
                         child: GridView.builder(
                           gridDelegate:
                               SliverGridDelegateWithFixedCrossAxisCount(
@@ -82,23 +137,132 @@ class GardenControl extends StatelessWidget {
                               icon: menuItem['icon'],
                               warna: menuItem['warna'],
                               onTap: () {
-                                Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                    builder: (context) => menuItem['route'],
-                                  ),
-                                );
+                                _toggleControl(menuItem['control']);
                               },
                             );
                           },
                         ),
                       ),
-                    ),
+                    ],
                   ),
                 ),
               ),
-            ],
+            ),
           ),
+          if (_showGardenLightsControl)
+            Positioned(
+              bottom: 50,
+              left: 20,
+              right: 20,
+              child: Container(
+                padding: EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                  color: Colors.white.withOpacity(0.8),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text(
+                      'Garden Lights Control',
+                      style:
+                          TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(height: 20),
+                    ElevatedButton(
+                      onPressed: _toggleGardenLights,
+                      child: Text(_isGardenLightsOn
+                          ? 'Turn Off Garden Lights'
+                          : 'Turn On Garden Lights'),
+                    ),
+                    SizedBox(height: 10),
+                    ElevatedButton(
+                      onPressed: _cancelControl,
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.red,
+                      ),
+                      child: Text('Cancel'),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          if (_showSprinklersControl)
+            Positioned(
+              bottom: 50,
+              left: 20,
+              right: 20,
+              child: Container(
+                padding: EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                  color: Colors.white.withOpacity(0.8),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text(
+                      'Sprinklers Control',
+                      style:
+                          TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(height: 20),
+                    ElevatedButton(
+                      onPressed: _toggleSprinklers,
+                      child: Text(_isSprinklersOn
+                          ? 'Turn Off Sprinklers'
+                          : 'Turn On Sprinklers'),
+                    ),
+                    SizedBox(height: 10),
+                    ElevatedButton(
+                      onPressed: _cancelControl,
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.red,
+                      ),
+                      child: Text('Cancel'),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          if (_showFountainControl)
+            Positioned(
+              bottom: 50,
+              left: 20,
+              right: 20,
+              child: Container(
+                padding: EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                  color: Colors.white.withOpacity(0.8),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text(
+                      'Fountain Control',
+                      style:
+                          TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(height: 20),
+                    ElevatedButton(
+                      onPressed: _toggleFountain,
+                      child: Text(_isFountainOn
+                          ? 'Turn Off Fountain'
+                          : 'Turn On Fountain'),
+                    ),
+                    SizedBox(height: 10),
+                    ElevatedButton(
+                      onPressed: _cancelControl,
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.red,
+                      ),
+                      child: Text('Cancel'),
+                    ),
+                  ],
+                ),
+              ),
+            ),
         ],
       ),
     );
--- lib/pages/salon/salon.dart
+++ lib/pages/salon/salon.dart
@@ -1,112 +1,84 @@
-// import 'package:flutter/material.dart';
-// import 'package:smart_home/components/mymenu.dart';
-// import 'package:smart_home/pages/ac_controll.dart';
-// import 'package:smart_home/pages/light_bulb_controll.dart';
-// import 'package:smart_home/pages/stove_controlle.dart';
-// // Importing the TopContainer component
-
-// class SalonControll extends StatelessWidget {
-//   final List<Map<String, dynamic>> menuItems = [
-//     {
-//       'title': 'Light Control',
-//       'icon': Icons.lightbulb,
-//       'warna': Colors.yellow,
-//       'route': LightControlScreen(),
-//     },
-//     {
-//       'title': 'AC Control',
-//       'icon': Icons.ac_unit,
-//       'warna': Colors.blue,
-//       'route': ACControlScreen(),
-//     },
-//     {
-//       'title': 'Stove Control',
-//       'icon': Icons.kitchen,
-//       'warna': Colors.red,
-//       'route': StoveControlScreen(),
-//     },
-//   ];
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       body: Column(
-//         children: [
-//           // Adding the TopContainer at the top
-
-//           const SizedBox(
-//             height: 10,
-//           ),
-//           Expanded(
-//             child: Container(
-//               width: double.infinity,
-//               decoration: const BoxDecoration(
-//                 color: Color.fromARGB(155, 2, 17, 23),
-//                 borderRadius: BorderRadius.only(topRight: Radius.circular(60)),
-//               ),
-//               child: Padding(
-//                 padding: const EdgeInsets.all(20.0),
-//                 child: GridView.builder(
-//                   gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-//                     crossAxisCount:
-//                         MediaQuery.of(context).size.width > 600 ? 3 : 2,
-//                     crossAxisSpacing: 10.0,
-//                     mainAxisSpacing: 10.0,
-//                   ),
-//                   itemCount: menuItems.length,
-//                   itemBuilder: (context, index) {
-//                     final menuItem = menuItems[index];
-//                     return MyMenu(
-//                       title: menuItem['title'],
-//                       icon: menuItem['icon'],
-//                       warna: menuItem['warna'],
-//                       onTap: () {
-//                         Navigator.push(
-//                           context,
-//                           MaterialPageRoute(
-//                               builder: (context) => menuItem['route']),
-//                         );
-//                       },
-//                     );
-//                   },
-//                 ),
-//               ),
-//             ),
-//           ),
-//         ],
-//       ),
-//     );
-//   }
-// }
-
 import 'package:flutter/material.dart';
 import 'package:smart_home/components/mymenu.dart';
-import 'package:smart_home/pages/ac_controll.dart';
-import 'package:smart_home/pages/light_bulb_controll.dart';
-import 'package:smart_home/pages/stove_controlle.dart';
 import 'dart:ui'; // Import the dart:ui library for ImageFilter
 
-class SalonControll extends StatelessWidget {
+class SalonControll extends StatefulWidget {
+  @override
+  _SalonControllState createState() => _SalonControllState();
+}
+
+class _SalonControllState extends State<SalonControll> {
   final List<Map<String, dynamic>> menuItems = [
     {
       'title': 'Light Control',
       'icon': Icons.lightbulb,
       'warna': Colors.yellow,
-      'route': LightControlScreen(),
+      'control': 'light',
     },
     {
       'title': 'AC Control',
       'icon': Icons.ac_unit,
       'warna': Colors.blue,
-      'route': ACControlScreen(),
+      'control': 'ac',
     },
     {
       'title': 'Stove Control',
       'icon': Icons.kitchen,
       'warna': Colors.red,
-      'route': StoveControlScreen(),
+      'control': 'stove',
     },
   ];
+
+  bool _showLightControl = false;
+  bool _showACControl = false;
+  bool _showStoveControl = false;
+  bool _isLightOn = false;
+  bool _isACOn = false;
+  bool _isStoveOn = false;
+
+  void _toggleControl(String control) {
+    setState(() {
+      if (control == 'light') {
+        _showLightControl = !_showLightControl;
+        _showACControl = false;
+        _showStoveControl = false;
+      } else if (control == 'ac') {
+        _showACControl = !_showACControl;
+        _showLightControl = false;
+        _showStoveControl = false;
+      } else if (control == 'stove') {
+        _showStoveControl = !_showStoveControl;
+        _showLightControl = false;
+        _showACControl = false;
+      }
+    });
+  }
+
+  void _toggleLight() {
+    setState(() {
+      _isLightOn = !_isLightOn;
+    });
+  }
+
+  void _toggleAC() {
+    setState(() {
+      _isACOn = !_isACOn;
+    });
+  }
+
+  void _toggleStove() {
+    setState(() {
+      _isStoveOn = !_isStoveOn;
+    });
+  }
+
+  void _cancelControl() {
+    setState(() {
+      _showLightControl = false;
+      _showACControl = false;
+      _showStoveControl = false;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -164,12 +136,7 @@ class SalonControll extends StatelessWidget {
                               icon: menuItem['icon'],
                               warna: menuItem['warna'],
                               onTap: () {
-                                Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                    builder: (context) => menuItem['route'],
-                                  ),
-                                );
+                                _toggleControl(menuItem['control']);
                               },
                             );
                           },
@@ -181,6 +148,116 @@ class SalonControll extends StatelessWidget {
               ),
             ),
           ),
+          if (_showLightControl)
+            Positioned(
+              bottom: 50,
+              left: 20,
+              right: 20,
+              child: Container(
+                padding: EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                  color: Colors.white.withOpacity(0.8),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text(
+                      'Light Control',
+                      style:
+                          TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(height: 20),
+                    ElevatedButton(
+                      onPressed: _toggleLight,
+                      child:
+                          Text(_isLightOn ? 'Turn Off Light' : 'Turn On Light'),
+                    ),
+                    SizedBox(height: 10),
+                    ElevatedButton(
+                      onPressed: _cancelControl,
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.red,
+                      ),
+                      child: Text('Cancel'),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          if (_showACControl)
+            Positioned(
+              bottom: 50,
+              left: 20,
+              right: 20,
+              child: Container(
+                padding: EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                  color: Colors.white.withOpacity(0.8),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text(
+                      'AC Control',
+                      style:
+                          TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(height: 20),
+                    ElevatedButton(
+                      onPressed: _toggleAC,
+                      child: Text(_isACOn ? 'Turn Off AC' : 'Turn On AC'),
+                    ),
+                    SizedBox(height: 10),
+                    ElevatedButton(
+                      onPressed: _cancelControl,
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.red,
+                      ),
+                      child: Text('Cancel'),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          if (_showStoveControl)
+            Positioned(
+              bottom: 50,
+              left: 20,
+              right: 20,
+              child: Container(
+                padding: EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                  color: Colors.white.withOpacity(0.8),
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text(
+                      'Stove Control',
+                      style:
+                          TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(height: 20),
+                    ElevatedButton(
+                      onPressed: _toggleStove,
+                      child:
+                          Text(_isStoveOn ? 'Turn Off Stove' : 'Turn On Stove'),
+                    ),
+                    SizedBox(height: 10),
+                    ElevatedButton(
+                      onPressed: _cancelControl,
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.red,
+                      ),
+                      child: Text('Cancel'),
+                    ),
+                  ],
+                ),
+              ),
+            ),
         ],
       ),
     );
